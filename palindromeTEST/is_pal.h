266e0438744f0be04e778da076ad2bed30db0ea7
diegoirra <dgirra@hotmail.com>
2017-10-27 17:46:17 -0300

    added palindromeTEST. Compila pero tira seg fault

---
+++ palindromeTEST/is_pal.h
@@ -0,0 +1,14 @@
+#ifndef __IS_PAL_MIPS32_TEST_H__
+#define __IS_PAL_MIPS32_TEST_H__
+
+/*
+ * >>>>>>>>>>> Implementada en ASM MIPS32
+ * Identifica, procesa e imprime los componentes lexicos que resulten ser palindromos.
+ * PARAMETROS: ifd y ofd los descriptores abiertos de los archivos deentrada y salida respectivamente.
+ * 			   ibytes y obytes describen los tama˜nos en bytes de las unidades de transferencia
+ * 			   de datos desde y hacia el kernel de NetBSD
+ * RETORNO: int booleano correspondiente al exito de la funcion.
+ */
+int palindrome();
+
+#endif
---
+++ palindromeTEST/tp1.c
@@ -0,0 +1,20 @@
+#define _POSIX_C_SOURCE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include "is_pal.h"
+
+#define SUCCESS 0
+#define ERROR_INPUT_FILE 1
+#define ERROR_OUTPUT_FILE 2
+
+
+int main(int argc, char** argv) {
+
+    printf("%d",palindrome());
+
+    return SUCCESS;
+}
