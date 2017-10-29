a246a308953be5a97a3293079d8f3bdc7c6c6240
diegoirra <dgirra@hotmail.com>
2017-10-29 19:28:49 -0300

    added putchTEST. Compila pero se me colgo al correr

---
+++ putchTEST/tp1.c
@@ -0,0 +1,23 @@
+
+#define _POSIX_C_SOURCE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include "putch.h"
+
+#define SUCCESS 0
+
+
+int main(int argc, char** argv) {
+
+	char* buf = "this is a putch test\n";
+
+	int v0 = putch(1, 21, buf);
+
+	printf("putch result: %d\n", v0);
+
+    return SUCCESS;
+}
