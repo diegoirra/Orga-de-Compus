baabd949efc8945da595ec9e1b1790389da2a162
Ivan Bacigaluppo <ivanbac_96@hotmail.com>
2017-11-14 12:47:12 -0300

    funciona bien para una palabra

---
+++ TEST/getwordlen.h
@@ -0,0 +1,13 @@
+/*
+ * getch.h
+ *
+ *  Created on: Oct 28, 2017
+ *      Author: d_irra
+ */
+
+#ifndef GETWORDLEN_H_
+#define GETWORDLEN_H_
+
+int getwordlen(char*);
+
+#endif /* GETWORDLEN_H_ */
---
+++ integrandoEnC/tp1.c
@@ -0,0 +1,34 @@
+#define _POSIX_C_SOURCE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include "getch.h"
+#include "putch.h"
+#include "mymalloc.h"
+
+#define SUCCESS 0
+#define ERROR_INPUT_FILE 1
+#define ERROR_OUTPUT_FILE 2
+#define BUFFER_TAM 5
+
+
+int main(int argc, char** argv) {
+
+    char *buffer = mymalloc(BUFFER_TAM);
+
+    printf("Ingresar chars. Tam = %d\n", BUFFER_TAM);
+
+    int v0 = getch(1, BUFFER_TAM, buffer);
+
+    printf("getch result: %d\n", v0);
+
+    v0 = putch(1, BUFFER_TAM, buffer);
+
+    printf("putch result: %d\n", v0);    
+
+
+    return SUCCESS;
+}
